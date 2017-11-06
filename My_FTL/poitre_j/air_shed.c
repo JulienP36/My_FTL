7fe360760f90799bcaf40fb561bf237c53fc9639
poitre_j <poitre_j@d3d95c30-db05-4b34-8216-71df339a4fcf>
2017-11-06 13:00:33 +0000

    commit avancement

--- My_FTL/poitre_j/air_shed.c
+++ My_FTL/poitre_j/air_shed.c
@@ -5,18 +5,18 @@
 ** Login   <poitre_j@etna-alternance.net>
 ** 
 ** Started on  Sat Nov  4 14:37:24 2017 POITREAU Julien
-** Last update Sat Nov  4 16:34:21 2017 POITREAU Julien
+** Last update Sat Nov  4 17:57:25 2017 POITREAU Julien
 */
 
 #include "ftl.h"
 
 t_ship		*create_ship()
 {
-  t_ship	*p_hull;
-
-  p_hull = malloc(sizeof(int));
   my_putstr("construction du vaisseau en cours...\n");
-  if (p_hull == NULL)
+  t_ship	*p_ship;
+
+  p_ship = malloc(sizeof(int));
+  if (p_ship == NULL)
     {
       my_putstr("le vaisseau n'a pas pu etre construit ");
       my_putstr("par manque de materiaux\n");
@@ -25,7 +25,30 @@ t_ship		*create_ship()
   else
     {
       my_putstr("amelioration du vaisseau termine!\n");
-      //*p_hull = 50;
+      p_ship->hull = 50;
+      p_ship->ptr_weapon = NULL;
     }
-  return (p_hull);
+  return (p_ship);
+}
+
+int	add_weapon_to_ship(t_ship *p_ship)
+{
+  my_putstr("ajout des armes en cours...\n");
+  t_weapon	*p_weapon;
+
+  p_weapon = malloc(sizeof(int));
+  if (p_weapon == NULL)
+    {
+      my_putstr("votre vaisseau a explose quand vous avez ");
+      my_putstr("tente d'ajouter les armes\n");
+      return (0);
+    }
+  else
+    {
+      my_putstr("les armes ont ete ajoutes avec succes!\n");
+      p_weapon->damage = 10;
+      p_weapon->system_state = my_strdup("online");
+      p_ship->ptr_weapon = p_weapon;
+    }
+  return (1);
 }
