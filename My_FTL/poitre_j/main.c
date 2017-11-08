91b81d71a88e67d06df914d516c72288ef0a58ac
poitre_j <poitre_j@d3d95c30-db05-4b34-8216-71df339a4fcf>
2017-11-08 08:02:38 +0000

    petit commit matinal

--- My_FTL/poitre_j/main.c
+++ My_FTL/poitre_j/main.c
@@ -5,7 +5,7 @@
 ** Login   <poitre_j@etna-alternance.net>
 ** 
 ** Started on  Sat Nov  4 16:37:18 2017 POITREAU Julien
-** Last update Tue Nov  7 19:44:38 2017 POITREAU Julien
+** Last update Tue Nov  7 21:55:18 2017 POITREAU Julien
 */
 
 #include "ftl.h"
@@ -30,14 +30,36 @@ void	game_test(t_ship *ptr_ship)
       my_putstr("||__SECTOR_");
       my_put_nbr(counter);
       my_putstr("__||");
-      my_putstr("\nHULL : ");
+      my_putstr("\n\n|| Hull : ");
       my_put_nbr(ptr_ship->hull);
-      //my_putstr("\nHULL : ");
-      //my_put_nbr(ptr_ship->hull);
+      my_putstr("\n\nWEAPONS STATE : ");
+      if (my_strcmp(ptr_ship->weapon->system_state, "online") == 0)
+	my_putstr_color("green", ptr_ship->weapon->system_state);
+      else
+	my_putstr_color("red", ptr_ship->weapon->system_state);
+      my_putstr("\n|| Weapons damage : ");
+      my_put_nbr(ptr_ship->weapon->damage);      
+      my_putstr("\n\nFTL_DRIVE STATE : ");
+      if (my_strcmp(ptr_ship->ftl_drive->system_state, "online") == 0)
+	my_putstr_color("green", ptr_ship->ftl_drive->system_state);
+      else
+	my_putstr_color("red", ptr_ship->ftl_drive->system_state);
+      my_putstr("\n|| Energy : ");
+      my_put_nbr(ptr_ship->ftl_drive->energy);
+      my_putstr("\n\nNAVIGATION TOOLS STATE : ");
+      if (my_strcmp(ptr_ship->ftl_drive->system_state, "online") == 0)
+	my_putstr_color("green", ptr_ship->nav_tools->system_state);
+      else
+	my_putstr_color("red", ptr_ship->nav_tools->system_state);
+      my_putstr("\n|| Sector : ");
+      my_put_nbr(ptr_ship->nav_tools->sector);
+      my_putstr("\n|| Evade : ");
+      my_put_nbr(ptr_ship->nav_tools->evade);
       my_putstr("\n\n>INPUT~> ");
       choice = readLine();
       my_putstr(choice);
       counter++;
+      ptr_ship->nav_tools->sector++;
     }
 }
 
